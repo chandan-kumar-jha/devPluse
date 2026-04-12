a3edb82a469856f43e20744e89b052bb4a70d7dc
corno25kumar-creator <corno25kumar@gmail.com>
2026-04-12 20:56:16 +0530

    dfgre

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const api = axios.create({
-    baseURL:import.meta.env.VITE_API_URL || "/api",
+    baseURL: "/api", //import.meta.env.VITE_API_URL || in dev test
     withCredentials:true,
      headers: {
     'Content-Type': 'application/json',
